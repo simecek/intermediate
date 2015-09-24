ed86b6ccccd96781099f16d31fe6deedc589b2c5
simecek <lamparna@gmail.com>
2015-09-24 18:44:14 -0400

    innitial commit

---
+++ R/mediation.scan.R
@@ -0,0 +1,73 @@
+#' Mediation Scan
+#'
+#' For a given QTL haplotype probabilities `Q` and target `T`, the function tries to add all
+#' possible mediators `M` and calculates LOD statistic. The low LOD value indicates `Q` and
+#' `T` are conditionally independent given `M`, i.e. M is a mediator of causal relationship
+#' for `Q` to T.
+#'
+#' @param target numeric vector with gene/protein expression
+#' @param mediator matrix, each column is one gene/protein's expression
+#' @param annotation data.frame with mediator annotation, must include columns "chr" and "pos"
+#' @param qtl.geno matrix, haplotype probabilities at QTL we try to mediate
+#' @param covar additive covariates
+#' @param method select a method to handle missing cases
+#' @param verbose if TRUE, display information about the progress
+
+#' @export
+
+mediation.scan <- function(target, mediator, annotation, qtl.geno, covar=NULL,
+                           method=c(), verbose=TRUE) {
+
+  # check input
+  stopifnot(NROW(target) == NROW(mediator))
+  stopifnot(NROW(annotation) == NCOL(mediator))
+  stopifnot(NROW(qtl.geno) == NROW(target))
+  stopifnot(is.null(covar) | NROW(target) == NROW(covar))
+  stopifnot(!any(is.na(covar)))
+  stopifnot(!any(is.na(qtl.geno)))
+  stopifnot(all(is.numeric(target)))
+  stopifnot(all(is.numeric(mediator)))
+  stopifnot(all(is.numeric(qtl.geno)))
+  stopifnot(all(is.numeric(covar)))
+  stopifnot(c("CHR", "POS") %in% toupper(names(annotation)))
+  method = match.arg(method)
+
+  # data preparation
+  mediator <- cbind(mediator) # to ensure 'mediator' is a matrix
+  N <- ncol(mediator) # number of points to scan
+  if (is.null(covar)) covar <- cbind(rep(1, N)) # if no covariates, use just intercept
+  LOD <- rep(NA, N) # prepare output
+
+  if (method == "double-lod-diff") {
+    LOD0 <- logLik(lm(target ~ covar + qtl.geno))/log(10) - logLik(lm(target ~ covar))/log(10)
+  }
+
+  # for-loop comparing M0: target~covar+mediator[,i] vs M1: target~covar+mediator[,i]+qtl.geno
+  for (i in 1:N) {
+    if (verbose & i %% 1000 == 0) print(i)
+    no.na <- !is.na(target) & !is.na(mediator[,i])
+    loglik0 <- logLik(lm(target[no.na] ~ covar[no.na,] + mediator[no.na,i]))
+    loglik1 <- logLik(lm(target[no.na] ~ covar[no.na,] + mediator[no.na,i] + qtl.geno[no.na,]))
+
+    if (method == "ignore" | (method == "double-lod-diff" & all(no.na))) {
+      # "double-lod-diff" for no missing observation is identical to "ignore"
+      LOD[i] <- (loglik1 - loglik0) / log(10)
+    } else {
+      loglik2 <- logLik(lm(target[no.na] ~ covar[no.na,]))
+      loglik3 <- logLik(lm(target[no.na] ~ covar[no.na,] + qtl.geno[no.na,]))
+
+      if (method == "lod-diff") {
+        LOD[i] <- (loglik3 - loglik2 - (loglik1-loglik0)) / log(10)
+      } else if (method == "double-lod-diff") {
+        LOD[i] <- LOD0 - (loglik3 - loglik2 - (loglik1-loglik0)) / log(10)
+      } else if (method == "lod-ratio") {
+        LOD[i] <- (loglik1-loglik0) / (loglik3 - loglik2)
+      }
+    }
+  }
+
+  output <- annotation
+  output$LOD <- LOD
+  class(output) <- c("mediation", "data.frame")
+  return(output)
+}
---
+++ R/plot.mediation.R
@@ -0,0 +1,91 @@
+#' Plots conditioned LOD scores against genome position
+#'
+#' For a given QTL haplotype probabilities `Q` and target `T`, the function tries to add all
+#' possible mediators `M` and calculates LOD statistic. The low LOD value indicates `Q` and
+#' `T` are conditionally independent given `M`, i.e. M is a mediator of causal relationship
+#' for `Q` to T.
+#'
+#' @param medresults
+#' @param chrlen length of chromosomes as in \code{org.Mm.egCHRLENGTHS}
+
+#' @export
+
+plot.mediation = function(medresults, chrlen = mouse.chrlen, ...){
+
+  #Check input
+  stopifnot(c("CHR", "POS","LOD") %in% toupper(names(medresults)))
+
+  names(medresults) = toupper(names(medresults))
+
+  #Find unique chromosomes in results input, then pull out their lengths and calculate midpoints for plotting
+  chrlen = org.Mm.egCHRLENGTHS
+  medresults$CHR = sub("X","20",medresults$CHR)
+  medresults$CHR = sub("Y","21",medresults$CHR)
+  medresults$CHR = sub("M","22",medresults$CHR)
+  toMatch <- unique(medresults$CHR)
+  toMatch = as.numeric(toMatch)
+  toMatch = toMatch[order(toMatch)]
+  toMatch = as.character(toMatch)
+  toMatch = sub("20","X",toMatch)
+  toMatch = sub("21","Y",toMatch)
+  toMatch = sub("22","M",toMatch)
+  chrlen = chrlen[match(toMatch,names(chrlen))]
+  medresults$CHR = sub("20","X",medresults$CHR)
+  medresults$CHR = sub("21","Y",medresults$CHR)
+  medresults$CHR = sub("22","M",medresults$CHR)
+  chrlen.copy = chrlen  #Create copy of chrlen
+
+
+  ### Create X-axis gmb values for mediators (e.g. proteins/RNA) genome positions
+  mb  = medresults$POS
+  if(max(mb) > 3000){
+    mb = medresults$POS*1e-6
+  }
+  gmb = mb
+  medresults$CHR = as.character(medresults$CHR)
+  unique.chr = unique(medresults$CHR)
+  unique.chr = sub("X","20",unique.chr)
+  unique.chr = as.numeric(unique.chr)
+  unique.chr = unique.chr[order(unique.chr)]
+  unique.chr = sub("20","X",unique.chr)
+  #chrlen = chrlen[names(chrlen) %in% unique.chr]
+  if(max(chrlen) > 200) {
+    chrlen = chrlen * 1e-6
+  } # if(max(chrlen) > 200)
+  chrlen = cumsum(chrlen)
+  chrlen = c(0, chrlen)
+  chrmid = chrlen[-length(chrlen)] + (diff(chrlen) * 0.5)
+  # Add the preceeding chromosome lengths to each SNP position.
+  for(c in 2:length(unique.chr)) {
+    rows = which(medresults$CHR == unique.chr[c])
+    gmb[rows] = gmb[rows] + chrlen[c]
+  } # for(c)
+  gmb.mediation = gmb
+  xmin.mediation = min(gmb.mediation)-30
+  xmax.mediation = max(gmb.mediation)+30
+  gmb.mediation.extended = c(xmin.mediation,gmb.mediation,xmax.mediation) #Used to extend the x-axis of the plot a bit
+
+
+  chrlen = chrlen.copy
+  if(max(chrlen) > 200) {
+    chrlen = chrlen * 1e-6
+  } # if(max(chrlen) > 200)
+  chrlen = cumsum(chrlen)
+  chrlen = c(0, chrlen)
+  chrmid = chrlen[-length(chrlen)] + (diff(chrlen) * 0.5)
+
+
+  #### Create the plot
+  par(font = 2, font.lab = 2, font.axis = 2, xaxs="i",las = 1, mar=c(3, 4, 3, 1) + 0.1)
+  plot(gmb.mediation.extended, c(0,medresults$LOD,0), col = 0, ylim = c(0, max(medresults$LOD)*1.05),ylab = "Conditioned LOD", xaxt = "n", xlab = "")
+  usr = par("usr")
+  rect(chrlen[2 * 1:(length(chrlen) * 0.5) - 1], usr[3],
+       chrlen[2 * 1:(length(chrlen) * 0.5)], usr[4],
+       border = "grey60", lty=3, col = rgb(0.96,0.96,0.96))
+  rect(usr[1], usr[3], usr[2], usr[4], border = "black")
+  points(gmb.mediation, medresults$LOD, type = "p", pch=21,lty = 1,lwd=1, cex=0.9, col="firebrick4") #dark red points
+  text(chrmid, 0.97 * usr[4], names(chrlen)[-1], cex=1)
+
+
+  return("Finished!")
+}
