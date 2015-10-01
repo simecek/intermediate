d8ed9f7e479784c71d41548f61f94efcfb4f7100
simecek <lamparna@gmail.com>
2015-10-01 13:42:16 -0400

    forcing iplot to use my color, mplot -> kplot

--- R/kplot.R
+++ R/kplot.R
@@ -1,4 +1,4 @@
-#' Interactive Plot of conditioned LOD scores
+#' Karl Broman's Interactive Plot of conditioned LOD scores
 #'
 #' For a given QTL haplotype probabilities `Q` and target `T`, the function tries to add all
 #' possible mediators `M` and calculates LOD statistic. The low LOD value indicates `Q` and
@@ -17,15 +17,15 @@
 #'                       annotation=Tmem68$annotation,
 #'                       covar=Tmem68$covar,
 #'                       qtl.geno=Tmem68$qtl.geno)
-#' mplot(med, symbol.col="Associated.Gene.Name")
+#' kplot(med, symbol.col="Associated.Gene.Name")
 #'
 #' # save the plot as HTML page
-#' pl <- mplot(med, symbol.col="Associated.Gene.Name")
+#' pl <- kplot(med, symbol.col="Associated.Gene.Name")
 #' tmpdir <- tempdir()
 #' htmlwidgets::saveWidget(pl, paste0(tmpdir, "/Tmem68.html"),
 #'                         selfcontained=FALSE, libdir = "_assets")
 
-mplot <- function(medresults, symbol.col="Associated.Gene.Name", chrlen=mouse.chrlen, ...){
+kplot <- function(medresults, symbol.col="Associated.Gene.Name", chrlen=mouse.chrlen, ...){
 
   names(medresults) = toupper(names(medresults))
   symbol.col =  toupper(symbol.col)
@@ -43,6 +43,7 @@ mplot <- function(medresults, symbol.col="Associated.Gene.Name", chrlen=mouse.ch
   unique.chr <- levels(factor(factor(medresults$CHR, levels=c(1:max.chr, "X", "Y", "M")))) # all chromosomes, ordered 1..max.chr,X,Y,M
   chrcolor <- rep(6, length(unique.chr))
   chrcolor[1:length(unique.chr) %% 2 == 0] <- 3
+  chrcolor <- factor(chrcolor, levels=1:6)
   names(chrcolor) <- unique.chr
 
   iplot(x=medresults$GMB, y=medresults$LOD,
