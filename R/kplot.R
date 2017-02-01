4cbbc7c2a8352aeb42f5ec309a7a1d409dd6845d
simecek <lamparna@gmail.com>
2017-02-01 16:34:16 -0500

    plotly is now used for as an interactive plotting library

--- R/kplot.R
+++ R/kplot.R
@@ -1,4 +1,4 @@
-#' Karl Broman's interactive plot of conditioned LOD scores
+#' Interactive plot of conditioned LOD scores
 #'
 #' Plot LOD statistics calculated by \code{mediation.scan} against
 #' genomic positions. Display a gene name when hovering a mouse over.
@@ -24,32 +24,31 @@
 #' pl <- kplot(med, symbol.col="symbol")
 #' tmpdir <- tempdir()
 #' htmlwidgets::saveWidget(pl, paste0(tmpdir, "/Tmem68.html"),
-#'                         selfcontained=FALSE, libdir = "_assets")
+#'                         selfcontained=TRUE)
 #'                         
 #' @export
 
 kplot <- function(med, symbol.col="symbol", chrlen=mouse.chrlen, ...){
-
+  
   names(med) = toupper(names(med))
   symbol.col =  toupper(symbol.col)
-
+  
   #Check input
   stopifnot(c("CHR", "POS","LOD") %in% names(med))
-  if (!require(qtlcharts, quietly=TRUE)) stop("Requires 'qtlcharts' package, use devtools::install_github('kbroman/qtlcharts') to get it.")
 
   if (!("GMB" %in% names(med)))
     med$GMB <- gmb.coordinates(med$CHR, med$POS, chrlen=chrlen)
   if (symbol.col %in% names(med)) symbols <- med[,symbol.col] else symbols <- med[,1]
-
+  
   # coloring of chromosomes
   max.chr <- max(as.numeric(med$CHR[grep("[0-9]+", med$CHR)])) # number of autosomes
   unique.chr <- levels(factor(factor(med$CHR, levels=c(1:max.chr, "X", "Y", "M")))) # all chromosomes, ordered 1..max.chr,X,Y,M
-  chrcolor <- rep(6, length(unique.chr))
-  chrcolor[1:length(unique.chr) %% 2 == 0] <- 3
+  chrcolor <- rep(1, length(unique.chr))
+  chrcolor[1:length(unique.chr) %% 2 == 0] <- 2
   chrcolor <- factor(chrcolor, levels=1:6)
   names(chrcolor) <- unique.chr
-
-  iplot(x=med$GMB, y=med$LOD,
-        indID=symbols, group = chrcolor[med$CHR], 
-        chartOpts = list(xlab="Genomic position (Mb)", ylab="LOD"),  ...)
+  
+  plot_ly(med, x=~GMB, y=~LOD, type = 'scatter', mode = 'markers',
+          hoverinfo = 'text', color = chrcolor[med$CHR],
+          text = ~paste(med$SYMBOL)) %>% layout(showlegend = FALSE)   
 }
